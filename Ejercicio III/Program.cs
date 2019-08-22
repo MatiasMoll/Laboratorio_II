bca3ff1ad0b1688c1ca75e311ce4f3298f9007f6
ZanoniCintia <cintu94@hotmail.com>
2019-08-21 22:05:09 -0300

    Ejercicio 3

--- Ejercicio III/Program.cs
+++ Ejercicio III/Program.cs
@@ -11,17 +11,44 @@ namespace Ejercicio_III
         static void Main(string[] args)
         {
             Console.Title = "Ejercicio Nro 3";
+            bool flag = false;
             string valorIngresado;
             int numero;
-            Console.Write("Ingrese un numero para mostrar los primos: ");
-            valorIngresado = Console.ReadLine();
-            if(int.TryParse(valorIngresado, out numero))
-            {
-                for(int i=0;i<numero;i++)
+            do { 
+                Console.Write("Ingrese un numero para mostrar los primos: ");
+                valorIngresado = Console.ReadLine();
+                if(int.TryParse(valorIngresado, out numero))
                 {
-                    
+                    if (numero <= 1)
+                    {
+                        Console.WriteLine("ERROR! El numero ingresado debe ser MAYOR a 1.");
+                    }
+                    else
+                    {
+                        int j;
+                        int i;
+                        for (i = 2; i <= numero;i++)
+                        {
+                            for(j = 2; j < i; j++)
+                            {
+                                if((i%j) == 0)
+                                {
+                                    break;
+                                }
+                            }
+                            if (i == j)
+                            {
+                                Console.WriteLine("{0} es primo.", i);
+                            }
+                        }
+                    }
+                    flag = true;
+                }else
+                {
+                    Console.WriteLine("ERROR! Ingrese un valor valido.");
                 }
-            }
+            }while (!flag);
+            Console.ReadKey();
         }
     }
 }
