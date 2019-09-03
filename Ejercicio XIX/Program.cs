14d8d6d9a7f2002ca00a0e4fb5eb80ac87880379
ZanoniCintia <cintu94@hotmail.com>
2019-09-03 18:18:39 -0300

    Ejercicio 19

---
+++ Ejercicio XIX/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_XIX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Sobrecarga de Operadores";
+            Sumador prueba = new Sumador();
+            Sumador pruebaDos = new Sumador();
+            long numero;
+            string palabra;
+            pruebaDos.Add(15);
+            prueba.Add(15);
+            Console.WriteLine(prueba + pruebaDos);
+            Console.WriteLine(prueba | pruebaDos);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio XIX/Sumador.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_XIX
+{
+    public class Sumador
+    {
+        private int cantidadSumas;
+
+        public void Add(int cantSumas)
+        {
+            this.cantidadSumas = cantSumas;
+        }
+        public void Add()
+        {
+            this.Add(0);
+        }
+        public long Sumar(long a, long b)
+        {
+            cantidadSumas++;
+            return a + b;
+        }
+        public string Sumar(string a, string b)
+        {
+            cantidadSumas++;
+            return a + b;
+        }
+        public static long operator +(Sumador s1, Sumador s2)
+        {
+            return s1.cantidadSumas + s2.cantidadSumas;
+        }
+        public static bool operator |(Sumador s1, Sumador s2)
+        {
+            bool retorno = false;
+            if(s1.cantidadSumas == s2.cantidadSumas)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+    }
+
+}
